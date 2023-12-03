24b5e0241dc38babbb5681957dbf1ba694758d6a
lucc <1832649614@qq.com>
2023-12-04 01:37:52 +0800

    更改页面启动

--- app/src/main/java/com/example/newactivity/MainActivity.java
+++ app/src/main/java/com/example/newactivity/MainActivity.java
@@ -11,45 +11,16 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "TAG" ;
-    private Button btn_1_first, btn_1_xiangmu;
+
+    private Button btn_1_shouye;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //绑定一个id
-        btn_1_first = findViewById(R.id.btn_1_first);
-        //显性试图点击跳转
-        btn_1_first.setOnClickListener(view -> {
+        btn_1_shouye = findViewById(R.id.btn_1_shouye);
+        btn_1_shouye.setOnClickListener(view -> {
             startActivity(new Intent(this, firstActivity.class));
-            //点击按钮提示  LENGTH_SHORT短暂显示 LENGTH_LONG 长时间显示
-            Toast.makeText(this, "跳转到下一页", Toast.LENGTH_SHORT).show();
-            Log.d(TAG,"123213");
-//             finish();  //结束活动
         });
-        btn_1_xiangmu = findViewById(R.id.btn_1_xiangmu);
-        btn_1_xiangmu.setOnClickListener(view -> {
-            startActivity(new Intent(this,SecondActivity.class));
-            this.finish();
-        });
-    }
-
-    public boolean OnCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    public boolean OnOptionsItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.add_item:
-                Toast.makeText(this, "添加组件", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this, "删除组件", Toast.LENGTH_LONG).show();
-                break;
-            default:
-        }
-        return true;
     }
 }
--- app/src/main/java/com/example/newactivity/firstActivity.java
+++ app/src/main/java/com/example/newactivity/firstActivity.java
@@ -2,14 +2,53 @@ package com.example.newactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class firstActivity extends AppCompatActivity {
-
+    private static final String TAG = "TAG" ;
+    private Button btn_2_first, btn_2_xiangmu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        //绑定一个id
+        btn_2_first = findViewById(R.id.btn_2_first);
+        //显性试图点击跳转
+        btn_2_first.setOnClickListener(view -> {
+            startActivity(new Intent(this, SecondActivity.class));
+            //点击按钮提示  LENGTH_SHORT短暂显示 LENGTH_LONG 长时间显示
+            Toast.makeText(this, "跳转到下一页", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "123213");
+//             finish();  //结束活动
+        });
+        btn_2_xiangmu = findViewById(R.id.btn_2_xiangmu);
+        btn_2_xiangmu.setOnClickListener(view -> {
+            startActivity(new Intent(this, SecondActivity.class));
+//           finish();
+        });
+    }
 
+    public boolean OnCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "添加组件", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "删除组件", Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
