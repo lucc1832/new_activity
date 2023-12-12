ce942d4ff7268c4d278aad695c3ec219300f132a
lucc <1832649614@qq.com>
2023-12-13 02:08:50 +0800

    java代码更新

--- app/src/main/java/com/example/newactivity/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/RegisterActivity.java
@@ -1,8 +1,9 @@
 package com.example.newactivity.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.newactivity.R;
 
@@ -12,5 +13,21 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId 是被选中的 RadioButton 的 ID
+                switch (checkedId) {
+                    case R.id.radio_Button_man:
+                        // 处理选项1被选中的情况
+                        break;
+                    case R.id.radio_Button_woman:
+                        // 处理选项2被选中的情况
+                        break;
+                }
+            }
+        });
+
     }
 }
