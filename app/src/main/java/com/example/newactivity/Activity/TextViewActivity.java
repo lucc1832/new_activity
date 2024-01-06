2102d4f17773dcb6c6a7392c801e7902bd8cf324
lucc <1832649614@qq.com>
2024-01-06 23:12:22 +0800

    TextView代码更新

--- app/src/main/java/com/example/newactivity/Activity/TextViewActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/TextViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.newactivity.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -8,13 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.newactivity.R;
 
 public class TextViewActivity extends AppCompatActivity {
-    TextView text_view_ellipsize;
+    TextView text_view_ellipsize, text_view_hello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view);
-        text_view_ellipsize =findViewById(R.id.text_view_ellipsize);
+        text_view_ellipsize = findViewById(R.id.text_view_ellipsize);
         //跑马灯效果获取焦点
+        // text_view_ellipsize.setSelected(true);
+
+        //getText() 获取文字。setText("hello") 设置文字
         text_view_ellipsize.setSelected(true);
+        String text_view_helloText = text_view_hello.getText().toString();
+        text_view_hello.setTextColor(Color.RED);
+        text_view_hello.setTextSize(25.0F);
     }
 }
