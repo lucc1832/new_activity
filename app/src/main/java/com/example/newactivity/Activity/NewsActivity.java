550d2e23becf397186a1656d8ebdc59c639455e5
lucc <1832649614@qq.com>
2024-01-09 23:51:51 +0800

    news代码更新

---
+++ app/src/main/java/com/example/newactivity/Activity/NewsActivity.java
@@ -0,0 +1,16 @@
+package com.example.newactivity.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newactivity.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
--- app/src/main/java/com/example/newactivity/Activity/ProjectActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/ProjectActivity.java
@@ -1,5 +1,6 @@
 package com.example.newactivity.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -8,12 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.newactivity.R;
 
 public class ProjectActivity extends AppCompatActivity {
-    Button btn_project_login, btn_project_TextView;
+    Button btn_project_news, btn_project_TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
-
+        btn_project_news = findViewById(R.id.btn_project_news);
+        btn_project_news.setOnClickListener(view -> {
+            startActivity(new Intent(this, NewsActivity.class));
+        });
     }
 }
