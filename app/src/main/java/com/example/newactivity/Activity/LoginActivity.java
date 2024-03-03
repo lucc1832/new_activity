ce8f1ef2160a4b8eeb20b05853769bafd0e7d6e8
lucc <1832649614@qq.com>
2024-03-03 14:56:44 +0800

    longin 测试

--- app/src/main/java/com/example/newactivity/Activity/LoginActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/LoginActivity.java
@@ -1,16 +1,42 @@
 package com.example.newactivity.Activity;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 import com.example.newactivity.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private EditText etAccount, etPassword;
+    private String username = "183264";
+    private String pass = "1832649614";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+        etAccount = findViewById(R.id.et_login_phone);
+        etPassword = findViewById(R.id.et_login_password);
+        btnLogin.setOnClickListener(view -> {
+            String account = etAccount.getText().toString();
+            String password = etPassword.getText().toString();
+            if (TextUtils.equals(account,username )){
+                if (TextUtils.equals(password,pass )){
+                    Toast.makeText(LoginActivity.this,"恭喜你登录成功",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(LoginActivity.this,"密码错误，请重试",Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(LoginActivity.this,"用户名错误，请重试",Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/newactivity/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/RegisterActivity.java
@@ -13,21 +13,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // checkedId 是被选中的 RadioButton 的 ID
-                switch (checkedId) {
-                    case R.id.radio_Button_man:
-                        // 处理选项1被选中的情况
-                        break;
-                    case R.id.radio_Button_woman:
-                        // 处理选项2被选中的情况
-                        break;
-                }
-            }
-        });
 
     }
 }
