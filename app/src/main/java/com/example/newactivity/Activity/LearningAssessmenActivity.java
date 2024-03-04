3fa773deaed74de59dbbfbd2d0232f29e5351b19
lucc <1832649614@qq.com>
2024-03-04 20:25:35 +0800

    硬件测试

--- app/src/main/java/com/example/newactivity/Activity/LearningAssessmenActivity.java
+++ app/src/main/java/com/example/newactivity/Activity/LearningAssessmenActivity.java
@@ -2,6 +2,7 @@ package com.example.newactivity.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,9 +34,33 @@ public class LearningAssessmenActivity extends AppCompatActivity {
         });
         btn_project_menu = findViewById(R.id.btn_learning_menu);
         btn_project_menu.setOnClickListener(view -> {
-            startActivity(new Intent(this,MenuActivity.class));
+            startActivity(new Intent(this, MenuActivity.class));
             finish();
         });
 
+    }
+
+    public void toBaidu(View view) {
+        Intent intent = new Intent();
+        //跳转到浏览器 访问url
+//        intent.setAction(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("http://www.baidu.com"));
+
+        //打开照相机
+//        intent.setAction("android.media.action.IMAGE_CAPTURE");
+
+        //打电话
+//        intent.setAction(Intent.ACTION_DIAL);
+//        intent.setData(Uri.parse("tel:10086"));
+
+        //发送短信,自定义发送短信的内容
+//        intent.setAction(Intent.ACTION_SENDTO);
+//        intent.setData(Uri.parse("smsto:10086"));
+//        intent.putExtra("sms_body","你好这是给你的短信");
+
+        //打开相册
+        intent.setAction("android.intent.action.PICK");
+        intent.setType("image/*");
+        startActivity(intent);
     }
 }
